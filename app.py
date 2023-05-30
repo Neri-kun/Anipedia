eae9f8a305feacb4808fbd9b17856549652e998a
Neri-kun <56407838+Neri-kun@users.noreply.github.com>
2023-05-30 11:03:38 +0300

    Improved interface 4.0

--- app.py
+++ app.py
@@ -17,62 +17,18 @@ global_results = []
 app = Flask(__name__, static_url_path='/static')
 #app.config['STATIC_URL'] = '/static'
 
-# Define the DBpedia endpoint
-#sparql = SPARQLWrapper("https://dbpedia.org/sparql")
 
 @app.route('/')
 def home():
 
-    # Define the SPARQL endpoint and query
-    endpoint_url = "http://dbpedia.org/sparql"
-
-    # Set the SPARQL query
-    query = """
-        SELECT DISTINCT ?anime (REPLACE(REPLACE(SUBSTR(STR(?anime), STRLEN("http://dbpedia.org/resource/") + 1), "_", " "), "\'", "") AS ?title) (GROUP_CONCAT(DISTINCT ?episode; separator=", ") AS ?episodes) ?date ?abstract ?genres (GROUP_CONCAT(DISTINCT REPLACE(STR(?studio), "http://dbpedia.org/resource/", ""); separator=", ") AS ?studios)
-        WHERE {
-                {
-                    SELECT DISTINCT ?anime (GROUP_CONCAT(DISTINCT CONCAT(UCASE(SUBSTR(REPLACE(LCASE(SUBSTR(STR(?genre), STRLEN("http://dbpedia.org/resource/")+1)), "_", " "), 1, 1)), SUBSTR(REPLACE(LCASE(SUBSTR(STR(?genre), STRLEN("http://dbpedia.org/resource/")+1)), "_", " "), 2)); separator=", ") AS ?genres)
-                    (GROUP_CONCAT(DISTINCT REPLACE(STR(?studio), "http://dbpedia.org/resource/", ""); separator=", ") AS ?studios)
-                        WHERE {
-                                ?anime rdf:type dbo:Anime.
-                                OPTIONAL { ?anime dbp:genre ?genre. }
-                                   
-                                }
-                }
-          ?anime rdf:type dbo:Anime ;
-                 dbo:abstract ?abstract;
-                 dbp:first ?date;
-                dbp:episodes ?episode.
-                 
-                OPTIONAL {
-                        ?anime dbp:studio ?studio.
-                }
-          FILTER (LANG(?abstract) = 'en')
-        }
-        ORDER BY DESC(?date)
-        LIMIT 12
-    """
-    sparql = SPARQLWrapper(endpoint_url)
-    sparql.setQuery(query)
-    sparql.setReturnFormat(JSON)
-    results = sparql.query().convert()
     urls = []
-    anime_results = results["results"]["bindings"]
-    for result in anime_results :
+    results = generate_query_results()["results"]["bindings"]
+
+    new_results = []
+    for result in results :
         result["title"]["value"] = result["title"]["value"].replace("'", "\\'")
         url = generate_url_by_anime_title(result["title"]["value"])
         urls.append(url)
-
-    query = """SELECT DISTINCT (REPLACE(STRAFTER(str(?genre), "http://dbpedia.org/resource/"), "_", " ") AS ?genre)
-        WHERE {
-          ?anime rdf:type dbo:Anime;
-            dbp:genre ?genre.
-          FILTER (!regex(?genre, '^".*@".*en$'))
-            }"""
-
-    new_results = []
-
-    for result in results["results"]["bindings"] :
 
         episodes_string = result["episodes"]["value"]  # Assuming it is a string like "1,2,3,4,5"
         episodes_list = episodes_string.split(",")  # Split the string by comma to get a list of strings
@@ -85,25 +41,13 @@ def home():
         except:
             new_results.append("")
 
-    for i in range(len(new_results)):
-        results["results"]["bindings"][i]["episodes"]["value"] = new_results[i]
+    results = [{**result, "episodes": {"value": new_results[i]}} for i, result in enumerate(results)]
 
-
-    sparql = SPARQLWrapper(endpoint_url)
-    sparql.setQuery(query)
-    sparql.setReturnFormat(JSON)
-    genres = sparql.query().convert()["results"]["bindings"]
-    filtered_genres = []
-
-    for genre in genres:
-        genre_value = genre["genre"]["value"]
-        if genre_value and genre_value.strip():
-            filtered_genres.append(genre_value)
-    print("Genres :")
+    genres = generate_query_results("","All genres")["results"]["bindings"]
     filtered_genres = list(filter(lambda genre: genre["genre"]["value"].strip(), genres))
 
 
-    return render_template("anime_website.html",results=results["results"]["bindings"], urls = urls, genres = filtered_genres)
+    return render_template("anime_website.html",results=results, urls = urls, genres = filtered_genres)
 
 
 #de refactorizat
@@ -127,7 +71,7 @@ def search():
 
 
 
-#@app.route('/search/<int:page>/<query>/<genre>/<date>/<endDate>', methods=['GET'])
+
 @app.route('/search/<int:page>/<query>/<genre>/<date>/<endDate>', methods=['GET'])
 @app.route('/search/<int:page>/<query>/<genre>/<endDate>',defaults={'date': ''}, methods=['GET'])
 @app.route('/search/<int:page>/<query>/<endDate>',defaults={'date': '', 'genre': ''}, methods=['GET'])
@@ -139,14 +83,8 @@ def searchPaginated(page, query, genre="", date="", endDate=""):
     print(page)
     print(query)
     print("Genre : " + genre)
-    #print("Genre : " + genre)
-    #print(genre)
-    # Perform SPARQL query and return results
-    sparql = SPARQLWrapper("http://dbpedia.org/sparql")
-    query_statement = generate_query(query, genre, date, endDate)
-    sparql.setQuery(query_statement)
-    sparql.setReturnFormat(JSON)
-    all_results = sparql.query().convert()['results']['bindings']
+
+    all_results =  generate_query_results(query, genre, date, endDate)['results']['bindings']
 
     # Calculate the number of results to display per page
     results_per_page = 10
@@ -172,7 +110,7 @@ def searchPaginated(page, query, genre="", date="", endDate=""):
 
     # Render the search results template with the current page, total number of pages, and results
     return render_template('search_results.html', results=results, current_page=page, num_pages=num_pages, has_prev=has_prev, has_next=has_next,
-                           query=query, page=page, date=date, endDate=endDate, genre=genre)#, pagination=pagination)
+                           query=query, page=page, date=date, endDate=endDate, genre=genre, total_results = total_results)
 
 
 
@@ -186,12 +124,7 @@ def destination(page,query, loop_index, genre="", date="", endDate=""):
     print(page)
     print(query)
     print(genre)
-    sparql = SPARQLWrapper("http://dbpedia.org/sparql")
-    query_statement = generate_query(query, genre,date, endDate)
-    sparql.setQuery(query_statement)
-    sparql.setReturnFormat(JSON)
-    all_results = sparql.query().convert()['results']['bindings']
-
+    all_results = generate_query_results(query, genre,date, endDate)['results']['bindings']
     # Calculate the number of results to display per page
     results_per_page = 10
 
@@ -208,11 +141,7 @@ def destination(page,query, loop_index, genre="", date="", endDate=""):
     num_pages = math.ceil(len(all_results) / results_per_page)
 
     print("Value of page before render: " + str(page))
-
-
     urls = generate_urls_by_anime_title(result["title"]["value"])
-
-
     episodes_string = result["episodes"]["value"]  # Assuming it is a string like "1,2,3,4,5"
     episodes_list = episodes_string.split(",")  # Split the string by comma to get a list of strings
     try:
@@ -221,9 +150,7 @@ def destination(page,query, loop_index, genre="", date="", endDate=""):
     except :
         total_episodes = ""
     print(total_episodes)  # Output the sum of the episodes
-
     result["episodes"]["value"] = total_episodes
-
 
     return render_template('destination.html', result=result, urls = urls, page = page, query = query, genre = genre, endDate = endDate)
 
@@ -238,75 +165,121 @@ def utility_processor():
 
 
 
-def generate_query(query_keyword, genre, startDate, endDate):
-    query = """
-              SELECT DISTINCT
-                  ?anime
-                      (REPLACE(REPLACE(SUBSTR(STR(?anime), STRLEN("http://dbpedia.org/resource/") + 1), "_", " "), "\'", "") AS ?title)
-                      ?genres
-                      (GROUP_CONCAT(DISTINCT REPLACE(STR(?studio), "http://dbpedia.org/resource/", ""); separator=", ") AS ?studios)
-                      (GROUP_CONCAT(DISTINCT ?title; separator=", ") AS ?titles)
-                      (GROUP_CONCAT(DISTINCT ?episode; separator=", ") AS ?episodes)
-                      (GROUP_CONCAT(DISTINCT ?first_release_date; separator=", ") AS ?first_release_dates)
-                      (GROUP_CONCAT(DISTINCT ?last_release_date; separator=", ") AS ?last_release_dates)
-                      (GROUP_CONCAT(DISTINCT CONCAT(UCASE(SUBSTR(REPLACE(LCASE(SUBSTR(STR(?network), STRLEN("http://dbpedia.org/resource/")+1)), "_", " "), 1, 1)), SUBSTR(REPLACE(LCASE(SUBSTR(STR(?network), STRLEN("http://dbpedia.org/resource/")+1)), "_", ","), 2)); separator=",") AS ?networks)
-                      ?homepage
-                      ?abstract
+def generate_query_results(query_keyword="", genre="", startDate="", endDate=""):
+
+
+    # Define the SPARQL endpoint and query
+    endpoint_url = "http://dbpedia.org/sparql"
+
+    sparql = SPARQLWrapper(endpoint_url)
+
+    #Set query according to use cases
+
+    if query_keyword != "":
+        query = """
+                  SELECT DISTINCT
+                      ?anime
+                          (REPLACE(REPLACE(SUBSTR(STR(?anime), STRLEN("http://dbpedia.org/resource/") + 1), "_", " "), "\'", "") AS ?title)
+                          ?genres
+                          (GROUP_CONCAT(DISTINCT REPLACE(STR(?studio), "http://dbpedia.org/resource/", ""); separator=", ") AS ?studios)
+                          (GROUP_CONCAT(DISTINCT ?title; separator=", ") AS ?titles)
+                          (GROUP_CONCAT(DISTINCT ?episode; separator=", ") AS ?episodes)
+                          (GROUP_CONCAT(DISTINCT ?first_release_date; separator=", ") AS ?first_release_dates)
+                          (GROUP_CONCAT(DISTINCT ?last_release_date; separator=", ") AS ?last_release_dates)
+                          (GROUP_CONCAT(DISTINCT CONCAT(UCASE(SUBSTR(REPLACE(LCASE(SUBSTR(STR(?network), STRLEN("http://dbpedia.org/resource/")+1)), "_", " "), 1, 1)), SUBSTR(REPLACE(LCASE(SUBSTR(STR(?network), STRLEN("http://dbpedia.org/resource/")+1)), "_", ","), 2)); separator=",") AS ?networks)
+                          ?homepage
+                          ?abstract
+                          
+                      WHERE {
                       
-                  WHERE {
-                  
-                                {
-                                    SELECT DISTINCT ?anime (GROUP_CONCAT(DISTINCT CONCAT(UCASE(SUBSTR(REPLACE(LCASE(SUBSTR(STR(?genre), STRLEN("http://dbpedia.org/resource/")+1)), "_", " "), 1, 1)), SUBSTR(REPLACE(LCASE(SUBSTR(STR(?genre), STRLEN("http://dbpedia.org/resource/")+1)), "_", " "), 2)); separator=", ") AS ?genres)
-                                    WHERE {
-                                        ?anime rdf:type dbo:Anime.
-                                        OPTIONAL { ?anime dbp:genre ?genre. }
-                                   
+                                    {
+                                        SELECT DISTINCT ?anime (GROUP_CONCAT(DISTINCT CONCAT(UCASE(SUBSTR(REPLACE(LCASE(SUBSTR(STR(?genre), STRLEN("http://dbpedia.org/resource/")+1)), "_", " "), 1, 1)), SUBSTR(REPLACE(LCASE(SUBSTR(STR(?genre), STRLEN("http://dbpedia.org/resource/")+1)), "_", " "), 2)); separator=", ") AS ?genres)
+                                        WHERE {
+                                            ?anime rdf:type dbo:Anime.
+                                            OPTIONAL { ?anime dbp:genre ?genre. }
+                                       
+                                        }
                                     }
-                                }
-                      ?anime rdf:type dbo:Anime.
-                      OPTIONAL {
-                          ?anime dbo:abstract ?abstract.
-                      }
-                      OPTIONAL {
-                          ?anime foaf:name ?title.
-                      }
-                      OPTIONAL { 
-                          ?anime dbo:network ?network.
-                      }
-                      OPTIONAL {
-                          ?anime dbp:episodes ?episode.
-                      }
-                      OPTIONAL {
-                          ?anime dbp:studio ?studio.
-                      }
-                      FILTER (LANG(?abstract) = "en")
-                      OPTIONAL {
-                          ?anime foaf:homepage ?homepage .
-                      }
-                      OPTIONAL {
-                          ?anime dbp:first ?first_release_date.
-                      }
-                      OPTIONAL {
-                          ?anime dbp:last ?last_release_date.
-                      }
-                      FILTER(REGEX(?anime, \'http://dbpedia.org/resource/.*""" + query_keyword + """.*\', "i"))
-                """
-
-    if startDate == "":
-        query += """\nFILTER (?first_release_date <= '""" + endDate + """'^^xsd:date)
-        """
-    else:
-        query += """\nFILTER (?first_release_date >= '""" + startDate + """'^^xsd:date && ?first_release_date <= '""" + endDate + """'^^xsd:date)
-        """
-
-    if genre != "All genres":
-        words = genre.split()
-        for word in words:
-            query+= """\nFILTER(CONTAINS(LCASE(STR(?genres)),'""" + word.lower() + """'))
+                          ?anime rdf:type dbo:Anime.
+                          OPTIONAL {
+                              ?anime dbo:abstract ?abstract.
+                          }
+                          OPTIONAL {
+                              ?anime foaf:name ?title.
+                          }
+                          OPTIONAL { 
+                              ?anime dbo:network ?network.
+                          }
+                          OPTIONAL {
+                              ?anime dbp:episodes ?episode.
+                          }
+                          OPTIONAL {
+                              ?anime dbp:studio ?studio.
+                          }
+                          FILTER (LANG(?abstract) = "en")
+                          OPTIONAL {
+                              ?anime foaf:homepage ?homepage .
+                          }
+                          OPTIONAL {
+                              ?anime dbp:first ?first_release_date.
+                          }
+                          OPTIONAL {
+                              ?anime dbp:last ?last_release_date.
+                          }
+                          FILTER(REGEX(?anime, \'http://dbpedia.org/resource/.*""" + query_keyword + """.*\', "i"))
                     """
-    query +="""\n}"""
 
-    return query
+        if startDate == "":
+            query += """\nFILTER (?first_release_date <= '""" + endDate + """'^^xsd:date)
+            """
+        else:
+            query += """\nFILTER (?first_release_date >= '""" + startDate + """'^^xsd:date && ?first_release_date <= '""" + endDate + """'^^xsd:date)
+            """
+
+        if genre != "All genres":
+            words = genre.split()
+            for word in words:
+                query+= """\nFILTER(CONTAINS(LCASE(STR(?genres)),'""" + word.lower() + """'))
+                        """
+        query +="""\n}"""
+
+    elif genre == "All genres":
+        query = """SELECT DISTINCT (REPLACE(STRAFTER(str(?genre), "http://dbpedia.org/resource/"), "_", " ") AS ?genre)
+               WHERE {
+                 ?anime rdf:type dbo:Anime;
+                   dbp:genre ?genre.
+                 FILTER (!regex(?genre, '^".*@".*en$'))
+                   }"""
+    else:
+        query = """
+              SELECT DISTINCT ?anime (REPLACE(REPLACE(SUBSTR(STR(?anime), STRLEN("http://dbpedia.org/resource/") + 1), "_", " "), "\'", "") AS ?title) (GROUP_CONCAT(DISTINCT ?episode; separator=", ") AS ?episodes) ?date ?abstract ?genres (GROUP_CONCAT(DISTINCT REPLACE(STR(?studio), "http://dbpedia.org/resource/", ""); separator=", ") AS ?studios)
+              WHERE {
+                      {
+                          SELECT DISTINCT ?anime (GROUP_CONCAT(DISTINCT CONCAT(UCASE(SUBSTR(REPLACE(LCASE(SUBSTR(STR(?genre), STRLEN("http://dbpedia.org/resource/")+1)), "_", " "), 1, 1)), SUBSTR(REPLACE(LCASE(SUBSTR(STR(?genre), STRLEN("http://dbpedia.org/resource/")+1)), "_", " "), 2)); separator=", ") AS ?genres)
+                          (GROUP_CONCAT(DISTINCT REPLACE(STR(?studio), "http://dbpedia.org/resource/", ""); separator=", ") AS ?studios)
+                              WHERE {
+                                      ?anime rdf:type dbo:Anime.
+                                      OPTIONAL { ?anime dbp:genre ?genre. }
+
+                                      }
+                      }
+                ?anime rdf:type dbo:Anime ;
+                       dbo:abstract ?abstract;
+                       dbp:first ?date;
+                      dbp:episodes ?episode.
+
+                      OPTIONAL {
+                              ?anime dbp:studio ?studio.
+                      }
+                FILTER (LANG(?abstract) = 'en')
+              }
+              ORDER BY DESC(?date)
+              LIMIT 12
+          """
+    sparql.setQuery(query)
+    sparql.setReturnFormat(JSON)
+    results = sparql.query().convert()
+    return results
 
 def get_original_image_size(image_url):
     response = requests.get(image_url, stream=True)
@@ -355,7 +328,6 @@ def generate_urls_by_anime_title(anime_title):
         image_urls.append(image_url)
 
     return image_urls
-
 
 
 if __name__ == '__main__':
